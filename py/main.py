f75001cdeb2f8ce708fc55fae4ab127765fee70b
Matt Dooner <matt@doonerconsulting.com>
2015-04-11 20:22:38 +0100

    Added JSON export and committed json and video outputs

--- py/main.py
+++ py/main.py
@@ -1,14 +1,59 @@
-import cv2
+import cv, cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import display as d
 import match as m
+import utils
+import json
+
+def formatMatchPoints(matchDict) :
+    mps = matchDict['matchDisparities']
+    out = [0] * len(mps)
+    for i, mp in enumerate(mps):
+        out[i] = formatMatchPoint(mp,matchDict)
+    return out
+
+def formatMatchPoint(mp, matchDict) :
+    fmt = {};
+    # print '### mp ###'
+    # print mp
+    x1,y1,x2,y2 = utils.extractPoints(matchDict, mp)
+    fmt['leftKp'] = {'x':x1, 'y':y1}
+    fmt['rightKp'] = {'x':x2, 'y':y2}
+    fmt['matchDistance'] = mp['match'].distance
+    return fmt
+
+def matchPointStats(mpsFmt) :
+    # print mpsFmt
+    mpsStats = {}
+    dists = [0] * len(mpsFmt)
+    for i, mpFmt in enumerate(mpsFmt):
+        dists[i] = mpFmt['matchDistance']
+    if(len(dists) > 0):
+        mpsStats['min'] = min(dists)
+        mpsStats['max'] = max(dists)
+        mpsStats['mean'] = np.mean(dists)
+        mpsStats['stdev'] = np.std(dists)
+    else:
+        mpsStats['min'] = 0
+        mpsStats['max'] = 0
+        mpsStats['mean'] = 0
+        mpsStats['stdev'] = 0
+    return mpsStats
 
 cap = cv2.VideoCapture('data/test4-slam.avi')
 count = 0
 
-while(cap.isOpened()):
+data = []
+totalMatches = 0
+globalMin = 10000
+globalMax = 0
+
+while(cap.isOpened() and count < 2):
     ret, frame = cap.read()
+
+    pos_frames = cap.get(cv.CV_CAP_PROP_POS_FRAMES)
+    pos_msec = cap.get(cv.CV_CAP_PROP_POS_MSEC)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -24,10 +69,50 @@ while(cap.isOpened()):
 
     match = m.match(left,right)
 
+    mobj = utils.Bunch(match)
+
+    numMatches = len(mobj.matchDisparities)
+    totalMatches = totalMatches + numMatches
+
+    dataItem = {
+        "frame":pos_frames,
+        "timestamp":pos_msec,
+        "numMatches":numMatches,
+        "totalMatches":totalMatches
+    }
+
+    dataItem['matches'] = formatMatchPoints(match)
+
+    dataItem['matchDistanceStats'] = matchPointStats(dataItem['matches'])
+    globalMin = min(globalMin, dataItem['matchDistanceStats']['min'])
+    globalMax = max(globalMax, dataItem['matchDistanceStats']['max'])
+
+    count = count + 1
+
+    data.append(dataItem)
+
     if d.DISPLAY() :
         d.displayDisparity(match)
         if cv2.waitKey(0) & 0xFF == ord('q'):
             break
+
+    print "Frame %d / %d" % (count,-1)
+
+out = {'dataMinMatchDistance':globalMin,
+    'dataMaxMatchDistance':globalMax}
+out['data'] = data
+
+myjson = json.dumps(out)
+
+txt = open("data/output.json", "w")
+txt.write(myjson)
+txt.close()
+
+myjson = json.dumps(out, sort_keys=True,indent=4,separators=(',',': '))
+
+txt = open("data/output.pretty.json", "w")
+txt.write(myjson)
+txt.close()
 
 cap.release()
 cv2.destroyAllWindows()
