20234972b5c0d571db0463a8122d41ab02caa45a
Matt Dooner <matt@doonerconsulting.com>
2015-04-11 17:58:43 +0100

    Cleanup, added a initial spec for the json output

---
+++ py/display.py
@@ -0,0 +1,159 @@
+import utils
+import cv2
+import numpy as np
+
+def DISPLAY():
+	return True
+
+def displayDisparity(matchDict):
+	if DISPLAY() :
+		img3 = drawDisparity(matchDict)
+		cv2.imshow('Disparity',img3)
+
+def displayKeypoints(matchDict):
+	if s.DISPLAY() :
+		m = utils.Bunch(matchDict)
+		leftKpImg = cv2.drawKeypoints(m.left,m.leftKp,None,(255,0,0),4)
+		rightKpImg = cv2.drawKeypoints(m.right,m.rightKp,None,(255,0,0),4)
+		img3 = arragePairs(leftKpImg,rightKpImg)
+		cv2.imshow('Keypoints',img3)
+
+def arrangePairs(img1, img2):
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1,:cols1,:] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2, img2, img2])
+
+    return out
+
+def drawDisparity(matchDict):
+
+    m = utils.Bunch(matchDict)
+
+    out = arrangePairs(m.left, m.right)
+    cols1 = m.left.shape[1]
+
+    if len(m.matchDisparities) == 0:
+    	return out
+
+    colormap = ColorMap((0,255,0),(0,0,255),m.minDisparity['disp'],m.maxDisparity['disp'])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for matchDisparity in m.matchDisparities:
+
+    	mat = matchDisparity['match']
+    	d = matchDisparity['disp']
+    	dx = matchDisparity['dx']
+    	dy = matchDisparity['dy']
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = m.leftKp[img1_idx].pt
+        (x2,y2) = m.rightKp[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        # print 'd: %.2f dx: %.2f dy: %.2f' % (d,dx,dy)
+        color = colormap[d]
+        cv2.putText(out, str(round(dx)), (int(x1),int(y1)), cv2.FONT_HERSHEY_PLAIN, 1, color)
+        #cv2.circle(out, (int(x1),int(y1)), 4, color, 1)   
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, color, 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), color, 1)
+
+    return out
+
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
+
+    This function takes in two images with their associated 
+    keypoints, as well as a list of DMatch data structure (matches) 
+    that contains which keypoints matched in which images.
+
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
+
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
+
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
+
+    out = arrangePairs(img1, img2)
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+
+    return out
+
+class ColorMap:
+    startcolor = ()
+    endcolor = ()
+    startmap = 0
+    endmap = 0
+    colordistance = 0
+    valuerange = 0
+    ratios = []    
+
+    def __init__(self, startcolor, endcolor, startmap, endmap):
+        self.startcolor = np.array(startcolor)
+        self.endcolor = np.array(endcolor)
+        self.startmap = float(startmap)
+        self.endmap = float(endmap)
+        if abs(self.startmap - self.endmap) < 0.00001:
+            self.endmap = self.endmap + 1
+        self.valuerange = float(self.endmap - self.startmap)
+        self.ratios = (self.endcolor - self.startcolor) / self.valuerange
+
+    def __getitem__(self, value):
+        color = tuple(self.startcolor + (self.ratios * (value - self.startmap)))
+        return (int(color[0]), int(color[1]), int(color[2]))
--- py/match.py
+++ py/match.py
@@ -1,190 +1,52 @@
-import settings as s
+import display as d
 import cv2
 import numpy as np
 import math
-import colormap as cm
 
 def match(left, right) :
-	leftKp, leftDes = doSurf(left)
-	rightKp, rightDes = doSurf(right)
-	
-	bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    leftKp, leftDes = doSurf(left)
+    rightKp, rightDes = doSurf(right)
+    
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
-	matches = bf.match(leftDes,rightDes)
+    matches = bf.match(leftDes,rightDes)
 
-	disps = disparity(left,leftKp,right,rightKp,matches)
-	print disps[0]
-	
-	# print 'min: ' + str(minDisp['disp'])
-	# print 'max: ' + str(maxDisp['disp'])
+    disps = disparity(left,leftKp,right,rightKp,matches)
+    disps = filterDisparities(disps)
+    
+    # for d in disps:
+    #     print 'dx: %.2f dy: %.2f d: %.2f matchDistance: %.2f' % (
+    #         d['dx'],d['dy'],d['disp'],d['match'].distance)
 
-	matches = sorted(matches, key = lambda x:x.distance)
+    minDisp, maxDisp = minMaxDisp(disps)
 
-#	img3 = drawMatches(left,leftKp,right,rightKp,matches[:10])
-	img3 = drawDisparity(left,leftKp,right,rightKp,disps)
+    matchDict = {'left':left,'leftKp':leftKp,'leftDes':leftDes,
+        'right':right,'rightKp':rightKp,'rightDes':rightDes,
+        'matchDisparities':disps,'minDisparity':minDisp,'maxDisparity':maxDisp};
+    
+    return matchDict
 
-	print matches[0]
-
-	if s.DISPLAY() :
-		leftKpImg = cv2.drawKeypoints(left,leftKp,None,(255,0,0),4)
-		rightKpImg = cv2.drawKeypoints(right,rightKp,None,(255,0,0),4)
-		cv2.imshow('Matches',img3)
-
-
-	# if s.DISPLAY() :
-    #     cv2.imshow('Left',left)
-    #     cv2.imshow('Right',right)
-    #     if cv2.waitKey(0) & 0xFF == ord('q'):
-    #         break
-
-def doSurf(img) :
-	surf = cv2.SURF(1000)
-	print surf.descriptorSize()
-	print surf.extended
-	kp, des = surf.detectAndCompute(img,None)
-	print len(kp)
-	return kp, des
-
-def minMaxDisp(disparities) :
-	if len(disparities) > 0:
-		minDisp = min(disparities, key = lambda d : math.fabs(d['disp']))
-		maxDisp = max(disparities, key = lambda d : math.fabs(d['disp']))
-		return minDisp, maxDisp
-	return None, None
-
-def drawDisparity(img1, kp1, img2, kp2, disparities):
-    # Create a new output image that concatenates the two images together
-    # (a.k.a) a montage
-    rows1 = img1.shape[0]
-    cols1 = img1.shape[1]
-    rows2 = img2.shape[0]
-    cols2 = img2.shape[1]
-
-    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
-
-    # Place the first image to the left
-    out[:rows1,:cols1,:] = np.dstack([img1, img1, img1])
-
-    # Place the next image to the right of it
-    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2, img2, img2])
-
-    oldSize = len(disparities)
-    disparities = filter(lambda disp: disp['disp'] > 140, disparities)
+def filterDisparities(disparities):
+    disparities = filter(lambda x: x['disp'] > 140, disparities)
     disparities = filter(lambda disp: disp['disp'] < 170, disparities)
     disparities = filter(lambda disp: abs(disp['dy']) < 1, disparities)
     disparities = filter(lambda disp: abs(disp['dy']) > -1, disparities)
-    print 'filtered %d of %d' % (oldSize - len(disparities), oldSize)
+    return disparities
 
-    if len(disparities) == 0:
-    	return out
+def doSurf(img) :
+    surf = cv2.SURF(1000)
+    kp, des = surf.detectAndCompute(img,None)
+    return kp, des
 
-    minDisp, maxDisp = minMaxDisp(disparities)
-    print 'min: %.2f max: %.2f' % (minDisp['disp'], maxDisp['disp'])
-
-    colormap = cm.ColorMap((0,255,0),(0,0,255),minDisp['disp'],maxDisp['disp'])
-
-    # For each pair of points we have between both images
-    # draw circles, then connect a line between them
-    for disparty in disparities:
-
-    	mat = disparty['match']
-    	d = disparty['disp']
-    	dx = disparty['dx']
-    	dy = disparty['dy']
-
-        # Get the matching keypoints for each of the images
-        img1_idx = mat.queryIdx
-        img2_idx = mat.trainIdx
-
-        # x - columns
-        # y - rows
-        (x1,y1) = kp1[img1_idx].pt
-        (x2,y2) = kp2[img2_idx].pt
-
-        # Draw a small circle at both co-ordinates
-        # radius 4
-        # colour blue
-        # thickness = 1
-        print 'd: %.2f dx: %.2f dy: %.2f' % (d,dx,dy)
-        color = colormap[d]
-        cv2.putText(out, str(round(dx)), (int(x1),int(y1)), cv2.FONT_HERSHEY_PLAIN, 1, color)
-        #cv2.circle(out, (int(x1),int(y1)), 4, color, 1)   
-        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, color, 1)
-
-        # Draw a line in between the two points
-        # thickness = 1
-        # colour blue
-        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), color, 1)
-
-    return out
-
-def drawMatches(img1, kp1, img2, kp2, matches):
-    """
-    My own implementation of cv2.drawMatches as OpenCV 2.4.9
-    does not have this function available but it's supported in
-    OpenCV 3.0.0
-
-    This function takes in two images with their associated 
-    keypoints, as well as a list of DMatch data structure (matches) 
-    that contains which keypoints matched in which images.
-
-    An image will be produced where a montage is shown with
-    the first image followed by the second image beside it.
-
-    Keypoints are delineated with circles, while lines are connected
-    between matching keypoints.
-
-    img1,img2 - Grayscale images
-    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
-              detection algorithms
-    matches - A list of matches of corresponding keypoints through any
-              OpenCV keypoint matching algorithm
-    """
-
-    # Create a new output image that concatenates the two images together
-    # (a.k.a) a montage
-    rows1 = img1.shape[0]
-    cols1 = img1.shape[1]
-    rows2 = img2.shape[0]
-    cols2 = img2.shape[1]
-
-    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
-
-    # Place the first image to the left
-    out[:rows1,:cols1,:] = np.dstack([img1, img1, img1])
-
-    # Place the next image to the right of it
-    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2, img2, img2])
-
-    # For each pair of points we have between both images
-    # draw circles, then connect a line between them
-    for mat in matches:
-
-        # Get the matching keypoints for each of the images
-        img1_idx = mat.queryIdx
-        img2_idx = mat.trainIdx
-
-        # x - columns
-        # y - rows
-        (x1,y1) = kp1[img1_idx].pt
-        (x2,y2) = kp2[img2_idx].pt
-
-        # Draw a small circle at both co-ordinates
-        # radius 4
-        # colour blue
-        # thickness = 1
-        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
-        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
-
-        # Draw a line in between the two points
-        # thickness = 1
-        # colour blue
-        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
-
-    return out
+def minMaxDisp(disparities) :
+    if len(disparities) > 0:
+        minDisp = min(disparities, key = lambda d : math.fabs(d['disp']))
+        maxDisp = max(disparities, key = lambda d : math.fabs(d['disp']))
+        return minDisp, maxDisp
+    return None, None
 
 def disparity(img1, kp1, img2, kp2, matches):
-    
+
     out = [0] * len(matches)
 
     # For each pair of points we have between both images
